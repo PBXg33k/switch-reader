87f990ed938f2ab404c60de88eb35c2a7f32c2c6
colin969 <colinb969@gmail.com>
2018-11-21 08:21:44 +0000

    Cleaned up .gitignore for public showing

--- testy/source/shared.cpp
+++
@@ -1,8 +0,0 @@
-#include "shared.h"
-
-SDL_Color COLOR_WHITE = {255,255,255};
-SDL_Color COLOR_BLACK = {0,0,0};
-SDL_Color COLOR_LIGHTGRAY = {40,40,40};
-SDL_Color COLOR_GRAY = {20,20,20};
-SDL_Color COLOR_DARKRED = {50, 0, 0};
-SDL_Color COLOR_RED = {80, 10, 10};
